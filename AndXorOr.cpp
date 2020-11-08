19f3cc0d0f7c34dec1985bfe913df12fbe4b2678
Bala Ganesh <balaganesh12.12@outlook.com>
2020-11-08 09:06:44 +0530

    Create AndXorOr.cpp

---
+++ AndXorOr.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+#include<algorithm>
+
+//efficient - O(n)
+int andXorOr(std::vector<int> numbers)
+{
+    std::stack<int> stack;
+    int maxValue = 0;
+    int result = 0;
+    
+    for(int i = 0; i < numbers.size(); i++)
+    {
+        while(!stack.empty())
+        {
+            result = stack.top() ^ numbers[i];
+            maxValue = std::max(result,maxValue);
+            if(numbers[i] < stack.top())
+            {
+                stack.pop();
+            }
+            else
+            {
+                break;
+            }
+        }
+        stack.push(numbers[i]);
+    }
+    return maxValue;
+}
+
+
+int main()
+{
+	std::vector<int> a{9,6,3,5,2};
+	std::cout << andXorOr(a);
+}
